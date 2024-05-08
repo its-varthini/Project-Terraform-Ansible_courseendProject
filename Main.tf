fc5d3338620c84c98f7795cc461f0ea91e5501b0
its-varthini <162994868+its-varthini@users.noreply.github.com>
2024-05-08 14:16:07 +0100

    Add files via upload

---
+++ Main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-04b70fa74e45c3917"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+ 
+
+  tags = {
+    Name = "Jenkins-sr"
+  }
+  root_block_device {
+    volume_size = 8
+  }
+}
