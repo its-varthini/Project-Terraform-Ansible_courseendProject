b075e015d0dcee517861ab37169e01662e3dca0d
its-varthini <162994868+its-varthini@users.noreply.github.com>
2024-09-21 16:42:00 +0100

    Create terraform-setup.sh

---
+++ terraform-setup.sh
@@ -0,0 +1,26 @@
+# Install gnupg, software-properties-common, and curl packages
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# Install the HashiCorp GPG key.
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Verify the key's fingerprint.
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+# Add the official HashiCorp repository to your system. The lsb_release -cs command finds the distribution release codename for your current system, such as buster, groovy, or sid.
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Download the package information from HashiCorp.
+sudo apt update
+
+# Install Terraform from the new repository.
+sudo apt-get install terraform
+
+# Verify that the installation
+terraform -help
